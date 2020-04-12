3500869c0670b0c108dd0c2c3b49e7e09279a7c4
Praveen Premaratne <praveen.premaratne@groupcdg.com>
2020-04-12 18:28:47 +0100

    Add provider implementation

---
+++ providers/doc.go
@@ -0,0 +1,6 @@
+/*
+Package providers implements the mechanism to load authentication providers to the system
+either built in authentication or custom once. These are external plugins, which can extend
+the application authentication capabilities beyond built in once.
+*/
+package providers
---
+++ providers/provider.go
@@ -0,0 +1,67 @@
+package providers
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/praveenprem/logging"
+	"github.com/praveenprem/project-neptune/codes"
+	"github.com/praveenprem/project-neptune/providers/github"
+)
+
+/**
+ * Package name: providers
+ * Project name: ProjectNeptune
+ * Created by: Praveen Premaratne
+ * Created on: 25/03/2020 20:09
+ */
+
+type (
+	//Provider defines the structure for provider specific configuration.
+	Provider struct {
+		Name          string
+		Configuration interface{}
+	}
+	KeyChain struct {
+		Username string   `json:"username"`
+		Keys     []string `json:"keys"`
+	}
+	provider interface {
+		Authenticate(username string) (users []KeyChain, err error)
+		valid()
+	}
+)
+
+//Authenticate defines the logic used to determine the authentication provider
+// and translation of the data between main application and provider
+func (p *Provider) Authenticate(username string) (users []KeyChain, err error) {
+	var keyChain []KeyChain
+
+	p.valid()
+	switch p.Name {
+	case "github":
+		logging.Info("GitHub provider detected")
+		if k, err := github.Call(username, p.Configuration); err != nil {
+			logging.Info(err.Error())
+		} else {
+			logging.Info("decoding provider results")
+			err := json.Unmarshal([]byte(k), &keyChain)
+			if err != nil {
+				logging.Error(err.Error())
+			}
+			logging.Info("decoding completed")
+		}
+	default:
+		logging.Warning("Invalid provider given")
+		return keyChain, errors.New(codes.CODE3)
+	}
+	return keyChain, nil
+}
+
+//valid define logic to validate if the provider configuration
+func (p *Provider) valid() {
+	logging.Info("validating provider configuration")
+	if p.Name != "" || p.Configuration != nil {
+		return
+	}
+	panic("provider name or configuration not found")
+}
